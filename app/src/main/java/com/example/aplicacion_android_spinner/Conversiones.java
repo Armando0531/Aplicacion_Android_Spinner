2105ced7e789bee56722265395c51ccf44c47ef2
Armando0531 <armandoenriquez0531@gmail.com>
2023-06-15 20:41:32 -0600

    Se hicieron los eventos para el primer Spiner

---
+++ app/src/main/java/com/example/aplicacion_android_spinner/Conversiones.java
@@ -0,0 +1,8 @@
+package com.example.aplicacion_android_spinner;
+
+public class Conversiones {
+
+    public double celsiusAFahrenheit(double celsius) {
+        return (celsius * 1.8f) + 32;
+    }
+}

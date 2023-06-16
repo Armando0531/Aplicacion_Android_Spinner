8d113bfae9202d99557a753c3647fc1d29f0ff0d
Armando0531 <armandoenriquez0531@gmail.com>
2023-06-15 20:48:16 -0600

    Se le dio funcionalidad al spiner 2

--- app/src/main/java/com/example/aplicacion_android_spinner/MainActivity.java
+++ app/src/main/java/com/example/aplicacion_android_spinner/MainActivity.java
@@ -10,13 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    DecimalFormat df=new DecimalFormat("###.00");
     Spinner spinnerOperaciones,spinerConvertir;
     EditText cajaPrimerNumero;
     EditText cajaRes;
     Conversiones calculos=new Conversiones();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +25,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter adaptador = ArrayAdapter.createFromResource(this, R.array.Converciones,
                 android.R.layout.simple_spinner_item);
         spinnerOperaciones=findViewById(R.id.spinerTipoDeGrado);
-
         spinnerOperaciones.setAdapter(adaptador);
         spinnerOperaciones=findViewById(R.id.spinerTipoDeGrado);
         spinnerOperaciones.setOnItemSelectedListener(this);
-
         spinerConvertir=findViewById(R.id.spinerConvercionA);
         spinerConvertir.setOnItemSelectedListener(this);
         cajaPrimerNumero=findViewById(R.id.CajaNumeroIngresado);
@@ -42,10 +41,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 android.R.layout.simple_spinner_item);
         ArrayAdapter contenidosinK = ArrayAdapter.createFromResource(this, R.array.ConvercionesSinK,
                 android.R.layout.simple_spinner_item);
-
-        if (parent.getId() == R.id.spinerTipoDeGrado) {
+        if(parent.getId()==R.id.spinerTipoDeGrado){
             System.out.println("Entro al spiner 1");
-            switch (parent.getItemAtPosition(position).toString()) {
+            switch (parent.getItemAtPosition(position).toString()){
                 case "C°":
                     spinerConvertir.setAdapter(contenidosinC);
                     break;
@@ -55,21 +53,40 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 case "K°":
                     spinerConvertir.setAdapter(contenidosinK);
                     break;
-
             }
-
         }
-
-        if (parent.getId() == R.id.spinerConvercionA) {
+        if(parent.getId()==R.id.spinerConvercionA){
             System.out.println("Entro en el spiner 2");
-            if (cajaPrimerNumero.getText().toString().isEmpty()) {
-                cajaPrimerNumero.setText(0 + "");
-            } else {
-                if (spinerConvertir.getSelectedItem().toString().equals("C°")) {
+            if(cajaPrimerNumero.getText().toString().isEmpty()){
+                cajaPrimerNumero.setText(0+"");
+                }
+                if(spinnerOperaciones.getSelectedItem().toString().equals("C°")){
+                    if(spinerConvertir.getSelectedItemId()==1){
+                        double cantidadC=Double.parseDouble(cajaPrimerNumero.getText().toString());
+                        double res=calculos.celsiusAFahrenheit(cantidadC);
+                        cajaRes.setText(df.format(String.valueOf(res)));
+                    }else if(spinerConvertir.getSelectedItemId()==2){
+                        double cantidadC=Double.parseDouble(cajaPrimerNumero.getText().toString());
+                        cajaRes.setText(df.format(String.valueOf(cantidadC+273.15)));
+                    }
+                }else if(spinnerOperaciones.getSelectedItem().toString().equals("F°")){
+                    if(spinerConvertir.getSelectedItemId()==1){
+                        double cantidadF=Double.parseDouble(cajaPrimerNumero.getText().toString());
+                        double res=(cantidadF-32)/1.8000;
+                        cajaRes.setText(res+"");
+                    }else if(spinerConvertir.getSelectedItemId()==2){
+                        double cantidadF=Double.parseDouble(cajaPrimerNumero.getText().toString());
+                        double res=((cantidadF-32)/1.8000)+273.15;
+                        cajaRes.setText(res+"");
+                    }
+                }else if (spinnerOperaciones.getSelectedItem().toString().equals("K°")){
+                    if(spinerConvertir.getSelectedItemId()==1){
 
+                    }else if(spinerConvertir.getSelectedItemId()==2){
+
+                    }
                 }
             }
-        }
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {}
